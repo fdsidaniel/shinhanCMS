976ba863f998df0e94230238b301d2000b3df4df
정예진 <a1zip@nate.com>
2024-03-21 13:30:14 +0900

    1

--- front/pages/pub/CMS/CMSHOM001M00.vue
+++ front/pages/pub/CMS/CMSHOM001M00.vue
@@ -3,7 +3,13 @@
   <div class="cms_main_wrap">
     
     <div class="main_top">
-        
+        <div class="state_tot">
+            <h2>기관현황</h2>
+            <div class="config">
+                <span class="org_cnt">999개 기관</span>
+                <nuxt-link to="" class="btn_org_cfg">기관 설정</nuxt-link>
+            </div>
+        </div>        
     </div>
 
   </div>
