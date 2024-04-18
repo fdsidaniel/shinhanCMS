a342cd23d54cd8c9c60b832f1975ddb78df5b534
정예진 <a1zip@nate.com>
2024-04-18 16:00:19 +0900

    test

---
+++ front/components/common/ComInput.vue
@@ -0,0 +1,17 @@
+﻿<template>
+    <input :value="modelValue" @input="update" />
+</template>
+
+<script setup>
+
+
+const props = defineProps({
+  modelValue: { type: String }
+})
+const emit = defineEmits(['input'])
+
+const update = e => {
+    context.emit('update:modelValue', e.target.value)
+};
+
+</script>
---
+++ front/pages/pub/sample.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <div class="title_box">
+            <h2>
+                샘플
+                <v-btn class="ico_bookmark" size="small" :class="{ 'on': bookMark }" @click="btnBookmark">즐겨찾기 설정</v-btn>
+                <span class="msg" :class="{ 'clear': bookMarkMsg }">{{ bookMarkTxt }}</span>
+            </h2>
+            <ul class="loc">
+                <li>홈</li>
+                <li>원화(펌뱅킹)</li>
+                <li>당행 이체 내역 조회</li>
+            </ul>
+            <v-btn class="btn_bookmark" size="small" :class="{ 'on': bookMark }" @click="btnBookmark">즐겨찾기</v-btn>
+        </div>
+
+        <div class="conbody">
+            <ComInput type="number" v-model="data" class="comipt" />
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+    .comipt{padding:5px;border:1px solid #000;}
+</style>
+
+<script setup >
+
+import ComInput from '@/components/common/ComInput.vue'
+
+const userOrgName = ref('')
+const userOrgNameRules = [
+  (v) => !!v || '사용이 불가능합니다. 다시 입력해주세요.',
+  // (v) => (v && v.length >= 16) || '영문, 숫자 대소문자 구분없이 6~16 자리',
+]
+
+</script> 
