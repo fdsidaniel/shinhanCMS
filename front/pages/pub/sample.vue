97876a9db98863e80a8f6a8ffb8ec8ef8372fb99
정예진 <a1zip@nate.com>
2024-04-24 17:44:25 +0900

    슬라이더 css 샘플

--- front/pages/pub/sample.vue
+++ front/pages/pub/sample.vue
@@ -11,6 +11,24 @@
         </div>
 
         <div class="conbody">
+
+            <div class="swiper_wrap">
+                <ul class="slide">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+                <ul class="slide">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+            </div>
+
             <ComInput 
                 v-model="inputValue" 
                 type="text" 
