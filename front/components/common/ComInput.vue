022065b052cc4436888cdf198d64accc836380f0
정예진 <a1zip@nate.com>
2024-04-24 17:22:35 +0900

    1

--- front/components/common/ComInput.vue
+++ front/components/common/ComInput.vue
@@ -1,17 +1,55 @@
 ﻿<template>
-    <input :value="modelValue" @input="update" />
+  <div>
+    <input 
+      :type="type" 
+      :placeholder="placeholder"
+      :value="value"      
+      @input="handleInput"
+    />
+    <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <script setup>
 
+import { ref, defineProps, defineEmits } from 'vue';
 
 const props = defineProps({
-  modelValue: { type: String }
-})
-const emit = defineEmits(['input'])
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  value: {
+    type: [String, Number],
+    default: ''
+  },
+  rules: {
+    type: Array,
+    default: () => []
+  }
+});
 
-const update = e => {
-    context.emit('update:modelValue', e.target.value)
+const emit = defineEmits(['update:value']);
+
+const inputValue = ref('');
+const errorMessage = ref('');
+
+const handleInput = (event) => {
+  const value = event.target.value;
+  const ruleError = props.rules.find(rule => !rule.validator(value));
+  console.log(ruleError.value+"////")
+  if (ruleError) {
+    errorMessage.value = ruleError.message;
+    console.log('1')
+  } else {
+    errorMessage.value = '';
+    emit('update:value', value);
+    console.log('2')
+  }
 };
 
 </script>
